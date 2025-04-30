66bb1446a3f9d4320c92c96959ad330ab0ebb3f5
allanbarbosa01 <crash171@gmail.com>
2025-04-30 16:22:09 -0300

    fix: corrige migration AddDataNascimentoToUsuario com alterações apenas na tabela Usuarios

--- Migrations/20250430175243_AddDataNascimentoToUsuario.cs
+++ Migrations/20250430175243_AddDataNascimentoToUsuario.cs
@@ -17,32 +17,6 @@ namespace LanceCerto.WebApp.Migrations
                 type: "datetime2",
                 nullable: false,
                 defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
-
-            migrationBuilder.AlterColumn<string>(
-                name: "Titulo",
-                table: "Imoveis",
-                type: "nvarchar(100)",
-                maxLength: 100,
-                nullable: false,
-                oldClrType: typeof(string),
-                oldType: "nvarchar(max)");
-
-            migrationBuilder.AlterColumn<string>(
-                name: "ImagemUrl",
-                table: "Imoveis",
-                type: "nvarchar(max)",
-                nullable: true,
-                oldClrType: typeof(string),
-                oldType: "nvarchar(max)");
-
-            migrationBuilder.AlterColumn<string>(
-                name: "Estado",
-                table: "Imoveis",
-                type: "nvarchar(2)",
-                maxLength: 2,
-                nullable: false,
-                oldClrType: typeof(string),
-                oldType: "nvarchar(max)");
         }
 
         /// <inheritdoc />
@@ -51,34 +25,6 @@ namespace LanceCerto.WebApp.Migrations
             migrationBuilder.DropColumn(
                 name: "DataNascimento",
                 table: "Usuarios");
-
-            migrationBuilder.AlterColumn<string>(
-                name: "Titulo",
-                table: "Imoveis",
-                type: "nvarchar(max)",
-                nullable: false,
-                oldClrType: typeof(string),
-                oldType: "nvarchar(100)",
-                oldMaxLength: 100);
-
-            migrationBuilder.AlterColumn<string>(
-                name: "ImagemUrl",
-                table: "Imoveis",
-                type: "nvarchar(max)",
-                nullable: false,
-                defaultValue: "",
-                oldClrType: typeof(string),
-                oldType: "nvarchar(max)",
-                oldNullable: true);
-
-            migrationBuilder.AlterColumn<string>(
-                name: "Estado",
-                table: "Imoveis",
-                type: "nvarchar(max)",
-                nullable: false,
-                oldClrType: typeof(string),
-                oldType: "nvarchar(2)",
-                oldMaxLength: 2);
         }
     }
 }
