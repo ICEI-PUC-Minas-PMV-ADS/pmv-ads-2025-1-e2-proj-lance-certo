1db5af02e21947aa4c63b33e7f02e62c7021172a
allanbarbosa01 <crash171@gmail.com>
2025-04-30 08:59:24 -0300

    feat: cria ViewModel e Controller de cadastro; adiciona DataNascimento à entidade Usuario

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using LanceCerto.WebApp.Models;
+using LanceCerto.WebApp.Data;
+
+namespace LanceCerto.WebApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly LanceCertoDbContext _context;
+
+        public AccountController(LanceCertoDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Cadastro()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Cadastro(CadastroViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var usuario = new Usuario()
+                {
+                    Nome = model.Nome,
+                    Email = model.Email,
+                    Senha = model.Senha,
+                    DataNascimento = model.DataNascimento
+                };
+                _context.Usuarios.Add(usuario);
+                _context.SaveChanges();
+
+                return RedirectToAction("Login");
+            }
+            return View(model);
+        }
+    }
+}
--- Models/Usuario.cs
+++ Models/Usuario.cs
@@ -6,6 +6,7 @@
         public string Nome { get; set; }
         public string Email { get; set; }
         public string Senha { get; set; }
+        public DateTime DataNascimento { get; set; }
         public string Estado { get; set; } // ATIVO, INATIVO, BLOQUEADO
         public bool EhVendedor { get; set; }
         public bool EhCorretor { get; set; }
